be8c3c435bfb6fe3240bd3bae6991db6ff3962df
MaxiStahl1992 <76917082+MaxiStahl1992@users.noreply.github.com>
2022-02-26 21:46:51 +0100

    navbar completed

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,14 +1,58 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { CgMenuRight, CgClose } from 'react-icons/cg'
 
 import Switch from '../toggle-switch/Switch'
 import './Navbar.scss';
 
 const Navbar = ({switchTheme}) => {
-  return (
+    const[toggle, setToggle] = useState(false);
+   
+    return (
       <>
-        <div className='app__navbar app__flex'>
-            <div onClick={switchTheme}>
+        <div className='app__navbar'>
+
+            {/* Logo */}
+            <div className="app__navbar-logo app__flex">
+                <a href='#home' className='head-text'><span>A</span>LINA<span>.</span></a>
+            </div>
+            
+
+            {/* Navigation on big screens */}
+            <ul className="app__navbar-links">
+                {['home', 'about', 'resume', 'files', 'testimonials', 'contact'].map((item) => (
+                    <li className="app__flex p-text" key={`link-${item}`}>
+                        <div></div>
+                        <a href={`#${item}`}>{item}</a>
+                    </li>
+                ))}
+            </ul>
+
+            {/* Mode Toggle */}
+            <div className='app__navbar-switch' onClick={switchTheme}>
                 <Switch/>
+            </div>
+
+            {/* Navigation on small screens */}
+            <div className="app__navbar-menu">
+                <CgMenuRight className={`${toggle === true ? 'inactive' : ''}`} onClick={()=> setToggle(true)}/>
+                <CgClose className={`${toggle === true ? '' : 'inactive'}`} onClick={()=> setToggle(false)}/>
+                {
+                    toggle && (   
+                        <div>        
+                            <ul className='app__navbar-links'>
+                                {['home', 'about', 'resume', 'files', 'testimonials', 'contact'].map((item) => (
+                                    <li key={item}>
+                                        <a href={`#${item}`} onClick={()=> setToggle(false)}>{item}</a>
+                                    </li>
+                                ))}
+
+                                <li onClick={switchTheme}>
+                                    <Switch/>
+                                </li>
+                            </ul>
+                        </div> 
+                    )
+                }
             </div>
         </div>
       </>
@@ -16,3 +60,4 @@ const Navbar = ({switchTheme}) => {
 }
 
 export default Navbar
+
