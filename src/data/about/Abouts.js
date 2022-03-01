7d4ae91f81716f77777ee84a00e76b24e883fb62
MaxiStahl1992 <76917082+MaxiStahl1992@users.noreply.github.com>
2022-03-01 11:29:38 +0100

    added styling for mobile devices

--- src/data/about/Abouts.js
+++ src/data/about/Abouts.js
@@ -2,18 +2,60 @@ import { images } from '../../constants';
 
 export const abouts = [
     {
-        title: 'Profile',
-        description: 'Get To Know Me',
-        img: images.darmsheim,
+        title: 'Motivation',
+        img: images.motivation,
+        description: [
+            {   
+                name: 'What gets me up in the morning',
+                text: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vitae nulla voluptas nobis? Adipisci vero ipsum ad repellat eos in quae quos ratione, iure, eligendi minus laborum quo facere provident nostrum!'
+            }
+        ]
     },
     {
-        title: 'Motivation',
-        description: 'Get To Know My Work Style',
-        img: images.motivation,
+        title: 'Profile',
+        img: images.darmsheim,
+        description: [
+            {
+                name: 'First Name',
+                text: 'Alina'
+            },
+            {
+                name: 'Last Name',
+                text: 'Buck'
+            },
+            {
+                name: 'Birthday',
+                text: 'June 20th 1996'
+            },
+            {
+                name: 'Location',
+                text: 'Germering'
+            },
+            {
+                name: 'Grew Up In',
+                text: 'Darmsheim'
+            },
+        ]
     },
     {
         title: 'Hobbies',
-        description: 'Get To Know What I Do In My Spare Time',
         img: images.hobbies,
+        description: [
+            {
+                name: 'Sports',
+                text: 'Cross Country Skiing, Hiking, Fitness'
+            },
+            {
+                name: 'Travelling',
+                secondName: 'Favorite Spot',
+                text: 'Rotorua, New Zealand'
+            },
+            {
+                name: 'Dogs',
+                secondName: 'Favorite Breed',
+                text: 'Golden Retriever',
+                secondText: 'Looking forward to soon being a dog mom!',
+            },
+        ]
     }
 ]
